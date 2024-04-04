8ac5a1108902f2a6be4e04f804ae677b78efc0ea
Devansh Shah <devansh981226@gmail.com>
2024-04-04 09:24:24 -0400

    nav header

--- frontend/src/components/headers/light.js
+++ frontend/src/components/headers/light.js
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import tw from "twin.macro";
 import styled from "styled-components";
-import { css } from "styled-components/macro"; //eslint-disable-line
+//import { css } from "styled-components/macro"; //eslint-disable-line
 
 import useAnimatedNavToggler from "../../helpers/useAnimatedNavToggler.js";
 
@@ -61,13 +61,13 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
    * This header component accepts an optionals "links" prop that specifies the links to render in the navbar.
    * This links props should be an array of "NavLinks" components which is exported from this file.
    * Each "NavLinks" component can contain any amount of "NavLink" component, also exported from this file.
-   * This allows this Header to be multi column.
+   * This allows this Header to be multi-column.
    * So If you pass only a single item in the array with only one NavLinks component as root, you will get 2 column header.
-   * Left part will be LogoLink, and the right part will be the the NavLinks component you
+   * Left part will be LogoLink, and the right part will be the NavLinks component you
    * supplied.
    * Similarly if you pass 2 items in the links array, then you will get 3 columns, the left will be "LogoLink", the center will be the first "NavLinks" component in the array and the right will be the second "NavLinks" component in the links array.
    * You can also choose to directly modify the links here by not passing any links from the parent component and
-   * changing the defaultLinks variable below below.
+   * changing the defaultLinks variable below.
    * If you manipulate links here, all the styling on the links is already done for you. If you pass links yourself though, you are responsible for styling the links or use the helper styled components that are defined here (NavLink)
    */
   const defaultLinks = [
@@ -119,7 +119,7 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
 /* The below code is for generating dynamic break points for navbar.
  * Using this you can specify if you want to switch
  * to the toggleable mobile navbar at "sm", "md" or "lg" or "xl" above using the collapseBreakpointClass prop
- * Its written like this because we are using macros and we can not insert dynamic variables in macros
+ * Its written like this because we are using macros, and we can not insert dynamic variables in macros
  */
 
 const collapseBreakPointCssMap = {
